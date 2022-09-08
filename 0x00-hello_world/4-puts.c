b3f933ca8c117811d6cc82bcf60c5fd0e4ca91a4
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-08 06:34:28 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- *main - a program that prints a sentence
- *prints a string using puts
+ *main -Entry point
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
-/**
- *@puts: the command to be used
- *Return: 0
- */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
