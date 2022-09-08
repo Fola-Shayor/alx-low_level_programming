cee996d8d19c3d3f184e364b652127d9552a7796
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-08 06:03:44 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,10 @@
  */
 int main(void)
 /**
- *main - checks if the program is printed
- *@puts: the command to be used 
- *return: 0 if a statement is printed
+ *@puts: the command to be used
+ *Return: 0 if a statement is printed
  */
 {
 puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+return (0);
 }
