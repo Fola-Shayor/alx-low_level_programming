ddaa5d3b55c8dd6146085de63ae614bbee6705f4
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-09 08:53:06 -0700

    if else loop

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,17 +9,13 @@
 int main(void)
 {
 int n;
- 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0) {
-printf("%d\n is positive", n);
-}
-else if (n ==0 ) {
-printf("%d\n is zero", n);
-}
-else {
-printf("%d\n is negative", n);
-}
+if (n > 0)
+printf("%d is positive", n);
+else if (n == 0)
+printf("%d is zero", n);
+else
+printf("%d is negative", n);
 return (0);
 }
