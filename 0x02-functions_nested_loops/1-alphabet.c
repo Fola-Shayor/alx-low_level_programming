ab25d4725db5acf0902cb86bd13368dc135a167a
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-15 07:09:39 -0700

    an abc function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *main - calls a function
+ *print_alphabet -prints the alphabets in lower case
  *
  *Return: Always 0
  */
-void print_alphabet(void) {
+void print_alphabet(void)
+{
 int x;
 for (x = 'a'; x <= 'z'; x++)
-  putchar(x);
+putchar(x);
+putchar('\n');
 }
 
