366b2d7c3df257b159f1733c8fbb07b9e2fa3aa6
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-15 07:27:33 -0700

    alphabet * 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints the alphabet ten times
+ *
+ *Return: void
+ */
+void print_alphabet_x10(void)
+{
+int x;
+int y;
+for (x = 0; x <= 9; x++)
+{
+for (y = 'a'; y <='z'; y++)
+{
+putchar(y);
+}
+putchar('\n');
+}
+}
