7387645de6da866b370ee9ae49c8ae7bdb2e9059
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-16 09:17:27 -0700

    add multiple files

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_most_numbers - prints numbers from 0 to 9 except 3 and 4
+ *@x: variable used to store values for looping
+ *Return: void
+ */
+void print_most_numbers(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+if ((x != 2 && x != 4))
+_putchar(x + '0');
+}
+_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main"
+/**
+ *print_diagonal - prints forward slashes n number of times
+ *@n: stores the number of times '\' will be printed
+ *@x: an integer
+ *Return: void
+ */
+void print_diagonal(int n)
+{
+int x;
+if (n <= 0)
+{
+_putchar('\n');
+}
+for ( x = 0; x < n; x++)
+{
+putchar(92);
+}
+_putchar('\n');
+}
