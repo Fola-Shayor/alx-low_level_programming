3f192ab01eae13761380367ea326d0b937e177bc
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-16 03:21:48 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "main.h"
+#include <ctype.h>
 /**
  *main - checks for upper case letters
- *
+ *@c: carries the value to be checked
  *Return: 1 (success) 0 otherwise
  */
 int _isupper(int c)
 {
-if ( is upper(c) )
+if (isupper(c))
 {
 return (1);
 }
