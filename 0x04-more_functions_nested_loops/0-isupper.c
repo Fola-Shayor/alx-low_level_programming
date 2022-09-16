a9b5e1c6ca4902e075f2fa08de2383c300463c25
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-15 22:52:28 -0700

    checking for uppercase letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - checks for upper case letters
+ *
+ *Return: 1 (success) 0 otherwise
+ */
+int _isupper(int c)
+{
+if ( is upper(c) )
+{
+return (1);
+}
+else
+return (0);
+}
