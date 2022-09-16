4e9c001a570e4eb2d97064d100890301871d8780
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-16 13:46:27 -0700

    fizz buzz update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
-#include "main.h"
 /**
  *main - prints fizz for multiples of 3 buzz for 5 and fizzbuzzfor 15
- *
- *Return: 
+ *@x: integer
+ *Return: 0
  */
 int main(void)
 {
   int x;
   for (x = 1; x <= 100; x++)
     {
-      if (x % 3 == 0 )
+      if ((x % 3 == 0) && (x % 5 == 0))
 	{
-	 _putchar("Fizz");
+	 printf("FizzBuzz");
 	}
       else if (x % 5 == 0)
 	{
-	  _putchar("Buzz");
+	  printf("Buzz");
 	}
-      else if ((x % 3 == 0 ) && (x % 5 == 0))
+      else if ((x % 3 == 0))
 	{
-	  _putchar("FizzBuzz")
+	  printf("Fizz")
 	    }
-      _putchar('\n');
+      if (x != 100)
+	{
+	  printf(" ");
+	}
     }
+  printf('\n');
 }
