2b476fa6342db395a9eaa3bcff76be0b8a5a3dd7
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-19 12:17:01 -0700

    modifying 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -20,9 +20,10 @@ void print_rev(char *s)
 {
 int i;
 int len;
-for (i = (len -i); i >= 0; i--)
+len = _strlen(s);
+for (i = (len -1); i >= 0; i--)
 {
-_putchar(*(str + i));
+_putchar(*(s + i));
 }
 _putchar('\n');
 }
