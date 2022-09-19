232e762ff25f6e9473228fdca76b773c42ee6ad5
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-19 00:13:37 -0700

    adding files 5-sign.c and 6-abs.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
-#include "main"
+#include "main.h"
 #include <ctype.h>
 /**
  *print_sign - checks if n is a sign
- *
+ *@n: an integer
  *Return: 1 if n > 0; 0 if n == 0; -1 if n < 0
  */
 int print_sign(int n)
 {
-  if (n > 1)
-    {
-      printf("+");
-      retunr (1);
+if (n > 0)
+{
+printf("+");
+return (1);
+}
+else if (n == 0)
+{
+printf("0");
+return (0);
+}
+else if (n < 0)
+{
+printf("-");
+return (-1);
+}
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - impersonate math abs function
+ *@n: an integer
+ *Return: 0 
+ */
+int _abs(int n)
+{
+if (x < 0)
+{
+x = x * -1;
+printf("%d\n", x);
+}
+else {
+printf("%d\n", x);
+}
+}
