05f72cfb4e5827dc7811240f29d869413225db3f
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-19 12:11:33 -0700

    adding 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,11 +2,27 @@
 #include "main.h"
 #include <string.h>
 /**
+ *_strlen - returns the length of a string
+ *@s: a character variable
+ *Return: nothing
+ */
+int _strlen(const char *s) 
+{
+  return (strlen(s)); 
+}
+
+/**
  *print_rev - prints the reverse of a string to the stdout
  *@s:a character
  *Return: nothing
  */
 void print_rev(char *s)
 {
-printf("%s\n", strrev(*s));
+int i;
+int len;
+for (i = (len -i); i >= 0; i--)
+{
+_putchar(*(str + i));
+}
+_putchar('\n');
 }
