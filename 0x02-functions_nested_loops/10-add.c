6028fb2e390ab0f02208f5148867040a85d385eb
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-26 13:54:23 -0700

    more changes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *add - returns the addition of two integers
+ *@x: first integer
+ *@y: second integer
+ *Return: x + y
+ */
+int add(int x, int y)
+{
+return (x + y);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_last_digit - prints the last digit of a number
+ *@n: a parameter
+ *Return: last digit
+ */
+int print_last_digit(int n)
+{
+return (n % 10);
+}
