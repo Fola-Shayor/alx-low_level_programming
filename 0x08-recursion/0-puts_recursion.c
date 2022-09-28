900c17e1a19da57a74ffe7e31d6d81bfd14366f9
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-28 05:18:22 -0700

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_puts_recursion - prints out a string followed by a new line using puts
+ *@s: a variable contining the string
+ *Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+puts(*s);
+}
