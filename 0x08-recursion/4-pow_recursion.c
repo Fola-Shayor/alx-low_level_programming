e921b5d2e8170c6003e3839ac277f8a105bfbbe9
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-28 05:59:06 -0700

    adding file 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+/**
+ *_pow_recursion - imtates the math power function
+ *@x: an integer
+ *@y: an =other integer
+ *Return: x^y
+ */
+int _pow_recursion(int x, int y)
+{
+if ( y < 0)
+{
+return (-1);
+}
+return (pow(x, y));
+}
