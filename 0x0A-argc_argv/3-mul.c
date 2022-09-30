f3e7a37f9bec97ae1096b4d6c6e88d63641e7fed
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-30 06:58:30 -0700

    adding 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - multiplies two numbers
+ *@argv: a string parameter
+ *@argc: an integer parameter
+ *Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+int i, mul = 0;
+if (argc < 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+for (i = 1; i < argc; i++)
+{
+mul *= atoi(argv[i]);
+printf("%d\n", mul);
+}
+return (0);
+}
+}
