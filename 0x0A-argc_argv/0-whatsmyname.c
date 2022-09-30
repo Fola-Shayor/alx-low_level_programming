fbb7f4701fb5051ca57e51d826a2ce47c54f6d69
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-09-30 05:36:05 -0700

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - prints the name of the program
+ *@argc: integer parameter
+ *@argv: a string parameter
+ *Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%s", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - prints the number of arguments passed into the program
+ *@argc: an integer
+ *@argv: a string parameter
+ *Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%d", argc);
+return (0);
+}
