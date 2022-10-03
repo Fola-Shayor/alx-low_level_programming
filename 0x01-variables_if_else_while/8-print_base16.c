a71a30c1c9fb8758332cf9d0b83cc4be12b237ef
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-10-03 07:58:45 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,9 +7,13 @@
 int main(void)
 {
 int x;
-for (x = '00'; x <= '0f'; x++)
+for (x = '0'; x < '9'; x++)
 {
 putchar(x);
+}
+for (i = 'a'; i <= 'f'; i++)
+{
+putchar(i);
 }
 putchar('/');
 return (0);
