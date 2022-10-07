6327469783df56f6fb2e5b10f9f35ce2b7d862f8
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-10-07 06:25:21 -0700

    making changes

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,11 +6,22 @@
  */
 void print_to_98(int n)
 {
-  for (n = n; n <= 98; n++)
-    {
-      _putchar(n + '0');
-      _putchar(',');
-      _putchar(' ');
-    }
-  _putchar('\n');
+if (n <= 98)
+{
+for (n = n; n <= 98; n++)
+{
+_putchar(n);
+_putchar(',');
+_putchar(' ');
+}
+}
+else
+{
+for (n = n; n <=98; n--)
+{
+_putchar(n);
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
 }
