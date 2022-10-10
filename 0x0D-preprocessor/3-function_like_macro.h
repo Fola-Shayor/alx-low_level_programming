bbdd3d3a323916cd1bccc141c3e2585f4baf2516
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-10-10 11:14:43 -0700

    final change

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,21 +1,4 @@
-#include "main.h"
 #ifndef _ABS_H
 #define _ABS_H
-#define ABS(x)
-/**
- *main - imitates the math function abs
- *@x: an integer
- *Return: absolute value of x
- */
-int main()
-{
-#ifdef (x > 0)
-  {
-    _putchar(x);
-  }
-  #else
-  {
-    x = -1 * x;
-  _putchar(x);
-}
+#define ABS(x) (((x) >= 0) ? (x) : -(x))
 #endif
