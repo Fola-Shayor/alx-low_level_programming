ed2a3a3302a67a9452365d90b03beed86f0a729a
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-10-10 12:51:05 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *struct dog - a struct containg a few element
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: dog's owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
