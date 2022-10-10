ea332528efd49d04a8c1d64dca47755521cd4883
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-10-10 13:31:13 -0700

    struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ *init_dog -initialize variable of type dog
+ *@d: pointer to struct dog
+ *@name: pointer to dog's name
+ *@age: dog's age
+ *@owner: pointer to dog's owner
+ *Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
+{
+*d->name = name;
+*d->age = age;
+*d->owner = owner;
+}
+}
