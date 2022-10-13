7cd9d56b343754c759178619d8179fa67e8c154a
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-10-13 13:33:51 -0700

    variadic function with separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
+ *print_numbers - prints numbers and separator
+ *@separator: a char
+ *@n: an int;
+ *@i: an int;
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 int i;
 va_list list;
 va_start(list, n);
-if (*seperator == NULL)
+if (*seperator != NULL)
 {
-return;
-}
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(list, int));
 printf("%c", *separator);
 }
+}
 va_end(list);
-printf("\n");
+putchar('\n');
 }
