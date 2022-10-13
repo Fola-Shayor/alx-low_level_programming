59c54a426e98baa26e3af6b98d66a244aa7d582c
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-10-13 12:26:50 -0700

    changes to 0-sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,14 +12,16 @@ int sum_them_all(const unsigned int n, ...)
 int sum = 0;
 unsigned int i;
 va_list list;
-if (n)
-{
+
 va_start(list, n);
+if (n == 0)
+{
+return (0);
+}
 for (i = 0; i < n; i++)
 {
 sum += va_arg(list, int);
 }
 va_end(list);
-}
 return (sum);
 }
