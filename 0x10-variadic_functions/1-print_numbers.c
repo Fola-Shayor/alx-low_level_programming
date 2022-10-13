a4803442775853407f80fdba8e45857905ddfdc0
Osikoya Mofolasayo <osikoyamofolasayo3@gmail.com>
2022-10-13 13:54:14 -0700

    more changes

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,7 +11,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 unsigned int i;
 va_list list;
 va_start(list, n);
-for (i = 0; i < n; i++)
+for (i = 0; i < (n - 1); i++)
 {
 if (separator !=  NULL)
 {
